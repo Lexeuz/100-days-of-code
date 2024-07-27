2e262da1cebc210a56352bf98319bb4dafc46f66
Alexander <alexandererasoa@gmail.com>
2024-07-27 17:23:30 +0200

    Modifications to the Blackjack game

--- Day11/main.py
+++ Day11/main.py
@@ -4,30 +4,26 @@ from art import logo
 import os
 
 
-def check_deck(deck):
-    """Checks if the player has an Ace and a 10 in his deck."""
-    if 11 in deck and 10 in deck:
-        return True
-    else:
-        return False
+def calculate_score(deck):
+    """Checks if the player has an Ace and a 10 in his deck or returns the sum of all cards in the deck."""
+    if sum(deck) == 21 and len(deck) == 2:
+        return 0
+    return sum(deck)
 
 
 def final_hand(user_cards, pc_cards):
     """Shows the final hand."""
-    print(f"    Your final hand: {user_cards}, final score: {sum_cards(user_cards)}")
-    print(f"    Computer's final hand: {pc_cards}, final score: {sum_cards(pc_cards)}")
-
-
-def sum_cards(deck):
-    total = 0
-    for i in deck:
-        total += i
-    return total
+    print(
+        f"    Your final hand: {user_cards}, final score: {calculate_score(user_cards)}"
+    )
+    print(
+        f"    Computer's final hand: {pc_cards}, final score: {calculate_score(pc_cards)}"
+    )
 
 
 def show_deck(user_cards, pc_cards):
     """Shows the current deck."""
-    print(f"    Your cards: {user_cards}, current score: {sum_cards(user_cards)}")
+    print(f"    Your cards: {user_cards}, current score: {calculate_score(user_cards)}")
     print(f"    Computer's first card: {pc_cards[0]}")
 
 
@@ -51,15 +47,11 @@ def blackjack():
                 user_cards, pc_cards
             )  # Shows the deck of both user player and pc.
 
-            if check_deck(user_cards):
+            if calculate_score(user_cards) == 0:
                 final_hand(user_cards, pc_cards)
                 print(f"You win!")
                 should_continue = False
-            elif check_deck(pc_cards):
-                final_hand(user_cards, pc_cards)
-                print(f"Pc got {sum_cards(pc_cards)}, you lose.")
-                should_continue = False
-            if sum_cards(user_cards) > 21:
+            if calculate_score(user_cards) > 21:
                 final_hand(user_cards, pc_cards)
                 print(f"You went over it, you lose:")
                 should_continue = False
@@ -67,13 +59,13 @@ def blackjack():
                 if input("Type 'y' to get another card, type 'n' to pass: ") == "y":
                     user_cards.append(random.choice(cards))
                 else:
-                    while sum_cards(pc_cards) <= 21:
+                    while calculate_score(pc_cards) <= 21:
                         pc_cards.append(random.choice(cards))
-                        if sum_cards(pc_cards) > 21:
+                        if calculate_score(pc_cards) > 21:
                             print("Computer went over it, you win!")
                             final_hand(user_cards, pc_cards)
                             should_continue = False
-                        elif sum_cards(pc_cards) == 21:
+                        elif calculate_score(pc_cards) == 21:
                             print("Computer's final score is better, you lose!")
                             final_hand(user_cards, pc_cards)
                             should_continue = False
