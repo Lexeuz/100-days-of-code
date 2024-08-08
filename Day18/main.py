2f3917d5a462e2b3c0429adefba3c24b799d86c0
Alexander <alexandererasoa@gmail.com>
2024-08-08 10:01:02 +0200

    Added the project of the day.

--- Day18/main.py
+++ Day18/main.py
@@ -1,9 +1,42 @@
 import colorgram
+import random
+from turtle import Turtle, Screen
 
-colors = colorgram.extract("image.jpg", 6)
-color_list = []
+# Extract colors from the image.
+colors = colorgram.extract("image.jpg", 20)
+rgb_colors = []
 
-for i in range(0, len(colors)):
-    color_list.append(colors[i].rgb)
+for color in colors:
+    r = color.rgb.r
+    g = color.rgb.g
+    b = color.rgb.b
+    rgb_colors.append((r, g, b))
 
-print(color_list[0])
+# print(rgb_colors)
+
+# # Project - The Hirst Painting Project.
+screen = Screen()
+screen.colormode(255)
+
+tim = Turtle()
+tim.penup()
+tim.hideturtle()
+tim.speed("fastest")
+
+pos_x = -230
+pos_y = -230
+tim.setx(pos_x)
+tim.sety(pos_y)
+
+count = 0
+for _ in range(100):
+    tim.color(random.choice(rgb_colors))
+    tim.dot(20)
+    tim.forward(50)
+    count += 1
+    if count >= 10:
+        pos_y += 50
+        tim.setpos(pos_x, pos_y)
+        count = 0
+
+screen.exitonclick()
