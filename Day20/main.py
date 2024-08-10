aefdeaddc1ad970f49a0dfcc2bf8de5d92db00ba
Alexander <alexandererasoa@gmail.com>
2024-08-10 20:13:02 +0200

    Adding collision, food and score

--- Day20/main.py
+++ Day20/main.py
@@ -1,5 +1,7 @@
-from turtle import Screen, Turtle
+from turtle import Screen
 from snake import Snake
+from food import Food
+from scoreboard import Scoreboard
 import time
 
 screen = Screen()
@@ -9,8 +11,10 @@ screen.title("Snake Game - Alexander E. Adarme")
 screen.tracer(0)
 
 snake = Snake()
-screen.listen()
+food = Food()
+scoreboard = Scoreboard()
 
+screen.listen()
 screen.onkey(key="w", fun=snake.up)
 screen.onkey(key="s", fun=snake.down)
 screen.onkey(key="a", fun=snake.left)
@@ -20,8 +24,28 @@ game_is_on = True
 while game_is_on:
     time.sleep(0.1)
     screen.update()
-
     snake.move()
 
+    # Detect collision with food.
+    if snake.head.distance(food) < 15:
+        food.refresh()
+        snake.extend()
+        scoreboard.increase_score()
+
+    # Detect collision with wall.
+    if (
+        snake.head.xcor() > 280
+        or snake.head.xcor() < -280
+        or snake.head.ycor() > 280
+        or snake.head.ycor() < -280
+    ):
+        game_is_on = False
+        scoreboard.game_over()
+
+    # Detect collision with tail.
+    for segment in snake.segments[1:]:
+        if snake.head.distance(segment) < 10:
+            game_is_on = False
+            scoreboard.game_over()
 
 screen.exitonclick()
