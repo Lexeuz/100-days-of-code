a93e443eb17e4864c7fcf0f2402f54c0f2ebe435
Alexander <alexandererasoa@gmail.com>
2024-08-17 23:38:16 +0200

    Progress Crossing Game.

--- Day22/main.py
+++ Day22/main.py
@@ -7,8 +7,14 @@ from scoreboard import Scoreboard
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.tracer(0)
+screen.listen()
+
+player = Player()
+scoreboard = Scoreboard()
 
 game_is_on = True
 while game_is_on:
+    screen.onkey(key="w", fun=player.move_forward)
+    player.check_finish()
     time.sleep(0.1)
     screen.update()
--- Day22/scoreboard.py
+++ Day22/scoreboard.py
@@ -1,5 +1,20 @@
+from turtle import Turtle
+
 FONT = ("Courier", 24, "normal")
 
 
-class Scoreboard:
-    pass
+class Scoreboard(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.hideturtle()
+        self.penup()
+        self.goto(-280, 250)
+        self.level = 1
+        self.show_scoreboard()
+
+    def show_scoreboard(self):
+        self.clear()
+        self.write(f"Level: {self.level}", align="left", font=FONT)
+
+    def update_score(self):
+        self.level += 1
