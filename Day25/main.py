8fd5dc80d77756b93bbc9c0a0122d2d039fdf80a
Alexander <alexandererasoa@gmail.com>
2024-10-20 15:38:10 +0200

    CSV

--- Day25/main.py
+++ Day25/main.py
@@ -1,1 +1,10 @@
-# Pandas
+import csv
+
+with open("weather_data.csv", "r") as data_file:
+    data = csv.reader(data_file)
+    next(data)
+    temperatures = []
+    for row in data:
+        temperatures.append(int(row[1]))
+
+    print(temperatures)
